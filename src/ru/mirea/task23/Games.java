f762c5eba8593585b9dcf4df618c261249ce492b
TheVengefulOne <shalomeenko53@gmail.com>
2022-12-23 00:47:24 +0300

    test 24

--- src/ru/mirea/task23/Games.java
+++ src/ru/mirea/task23/Games.java
@@ -10,48 +10,52 @@ public class Games {
         this.name = name;
     }
     public int hashCode() {
-        int result = developer == null ? 0 : developer.hashCode();
-        result = 31 * result + year;
-        return result;
+        return this.year % 1998;
+
     }
+//    public int hashCode() {
+//        int result = developer == null ? 0 : developer.hashCode();
+//        result = 31 * result + year;
+//        return result;
+//    }
 
     public static void main(String[] args){
-        Games game1 = new Games(2011, "Blizzard Entertainment", "Warcraft 3: Reign of Chaos");
-        Games game2 = new Games(2002, "Valve", "Portal 2");
-        Games game3 = new Games(2014, "Blizzard Entertainment", "Hearthstone");
+        Games game1 = new Games(2012, "Arkane Studios", "Dishonored");
+        Games game2 = new Games(2011, "Valve", "Portal 2");
+        Games game3 = new Games(2012, "Blizzard Entertainment", "Diablo 3");
         Games game4 = new Games(1999, "The 3DO Company", "Heroes of Might and Magic 3"); //самая лучшая по мнению автора
-        Games game5 = new Games(2012, "Arkane Studios", "Dishonored");
+        Games game5 = new Games(2002, "Blizzard Entertainment", "Warcraft 3: Reign of Chaos");
 
-        if(game1.hashCode()==game1.hashCode()) {
-            System.out.println("Game " + game1.developer + " " + game1.name + " is equal to the game " + game5.developer + " " + game5.name);
+        if(game1.hashCode()==game5.hashCode()) {
+            System.out.println("Game " + game1.name + " by " + game1.developer + " is equal to game " + game5.name + " by " + game5.developer);
         }
-        else System.out.println("Game " + game1.developer + " " + game1.name + " not equal to game " + game5.developer + " " + game5.name);
-        System.out.println("Hash code of the  game " + game1.developer + " " + game1.name+  ": " + game1.hashCode());
-        System.out.println("Hash code of the  game " + game5.developer + " " + game5.name+  ": " + game5.hashCode());
+        else System.out.println("Game " + game1.name + " by " + game1.developer + " is not equal to game " + game5.name + " by " + game5.developer);
+        System.out.println("Hash code of the game " + game1.name + " by " + game1.developer+  ": " + game1.hashCode());
+        System.out.println("Hash code of the game " + game5.name + " by " + game5.developer+  ": " + game5.hashCode());
         System.out.println("");
 
         if(game1.hashCode()==game2.hashCode()) {
-            System.out.println("Game " + game1.developer + " " + game1.name + " is equal to the game " + game2.developer + " " + game2.name);
+            System.out.println("Game " + game1.name + " by " + game1.developer + " is equal to game " + game2.name + " by " + game2.developer);
         }
-        else System.out.println("Game " + game1.developer + " " + game1.name + " not equal to game " + game2.developer + " " + game2.name);
-        System.out.println("Hash code of the  game " + game1.developer + " " + game1.name+  ": " + game1.hashCode());
-        System.out.println("Hash code of the  game " + game2.developer + " " + game2.name+  ": " + game2.hashCode());
+        else System.out.println("Game " + game1.name + " by " + game1.developer + " is not equal to game " + game2.name + " by " + game2.developer);
+        System.out.println("Hash code of the game " + game1.name + " by " + game1.developer+  ": " + game1.hashCode());
+        System.out.println("Hash code of the game " + game2.name + " by " + game2.developer+  ": " + game2.hashCode());
         System.out.println("");
 
         if(game1.hashCode()==game3.hashCode()) {
-            System.out.println("Game " + game1.developer + " " + game1.name + " is equal to the game " + game3.developer + " " + game3.name);
+            System.out.println("Game " + game1.name + " by " + game1.developer + " is equal to game " + game3.name + " by " + game3.developer);
         }
-        else System.out.println("Game " + game1.developer + " " + game1.name + " not equal to game " + game3.developer + " " + game3.name);
-        System.out.println("Hash code of the  game " + game1.developer + " " + game1.name+  ": " + game1.hashCode());
-        System.out.println("Hash code of the  game " + game3.developer + " " + game3.name+  ": " + game3.hashCode());
+        else System.out.println("Game " + game1.name + " by " + game1.developer + " is not equal to game " + game3.name + " by " + game3.developer);
+        System.out.println("Hash code of the game " + game1.name + " by " + game1.developer+  ": " + game1.hashCode());
+        System.out.println("Hash code of the game " + game3.name + " by " + game3.developer+  ": " + game3.hashCode());
         System.out.println("");
 
         if(game1.hashCode()==game4.hashCode()) {
-            System.out.println("Game " + game1.developer + " " + game1.name + " is equal to the game " + game4.developer + " " + game4.name);
+            System.out.println("Game " + game1.name + " by " + game1.developer + " is equal to game " + game4.name + " by " + game4.developer);
         }
-        else System.out.println("Game " + game1.developer + " " + game1.name + " not equal to game " + game4.developer + " " + game4.name);
-        System.out.println("Hash code of the  game " + game1.developer + " " + game1.name+  ": " + game1.hashCode());
-        System.out.println("Hash code of the  game " + game5.developer + " " + game4.name+  ": " + game4.hashCode());
+        else System.out.println("Game " + game1.name + " by " + game1.developer + " is not equal to game " + game4.name + " by " + game4.developer);
+        System.out.println("Hash code of the game " + game1.name + " by " + game1.developer+  ": " + game1.hashCode());
+        System.out.println("Hash code of the game " + game4.name + " by " + game4.developer+  ": " + game4.hashCode());
 
 
     }
