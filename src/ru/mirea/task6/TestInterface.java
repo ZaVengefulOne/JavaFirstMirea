6fb2df366d4dce2d8cfad77e14955aae73136b8b
TheVengefulOne <shalomeenko53@gmail.com>
2022-12-25 21:38:27 +0300

    test 6

--- src/ru/mirea/task6/TestInterface.java
+++ src/ru/mirea/task6/TestInterface.java
@@ -2,7 +2,7 @@ package ru.mirea.task6;
 
 public class TestInterface {
     public static void main(String[] args) {
-        Priceable p = new Excursion("Tripster tour in Zelenogradsk", 8298, 1.205);
+        Priceable p = new Excursion("Krelin tour", 8298, 1.205);
         System.out.println(p);
         System.out.println(p.getPrice());
         p = new Laptop(124041, "HP ProBook 450 G8");
